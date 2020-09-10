bb2fdf9c701fc3970729dd970b058dfc411ba84a
Psycoxer <oajrockstar@gmail.com>
2020-09-10 16:00:06 +0530

    build added

---
+++ build/precache-manifest.faedb4e063192cead78c1de4333fb3e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf34ebdf652b6a3a01ad8693e2178b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e52477e6b3c265e3dce4",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8247fed2bfce0b338f47",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e52477e6b3c265e3dce4",
+    "url": "/static/js/2.895c4939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.895c4939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8247fed2bfce0b338f47",
+    "url": "/static/js/main.bc5cc1ed.chunk.js"
+  },
+  {
+    "revision": "a09792300aedfe1060d4",
+    "url": "/static/js/runtime-main.d8921639.js"
+  }
+]);
